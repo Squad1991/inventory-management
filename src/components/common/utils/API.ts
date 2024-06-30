f1394185959837bf2630024bebc6a5b8f618e1df
Dipak Ingole (dingole) <dipak.a.ingole@gmail.com>
2024-06-30 13:32:50 +0530

    Update API.ts
    Remove token

--- src/components/common/utils/API.ts
+++ src/components/common/utils/API.ts
@@ -7,7 +7,7 @@ const fetchConfig = {
   headers: {
     accept: 'application/json',
     Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3M2E4MDhlMTE2MDI1MThiZTFiNjAzZDE1ZGY5ZTM2OSIsInN1YiI6IjY2NjRiMGUyNTQ2YmNiM2ZlYWVmZDgwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ouqmq0Mk92qRXs5mhCB-LW2jhp2oyXBSF0ORKvr4AJ8',
+      '__YOUR-API-TOKEN__', // https://www.themoviedb.org/settings/api
   },
 };
 
